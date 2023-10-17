a559fb6920441d946cba1e921d6e6b32bd08b52a
Unmsandhiya <147695019+Unmsandhiya@users.noreply.github.com>
2023-10-17 13:34:31 +0530

    Add files via upload

---
+++ challenge1.2.py
@@ -0,0 +1,12 @@
+year=int(input("Enter the year:"))
+
+if (year%4==0):
+    if(year%100==0):
+     if(year%400):
+      print(year," is a leap year")
+else:
+ print(year, "is a not leap year")
+
+
+print(2016,"is leap year ")
+print(year,"Not a leap year")
